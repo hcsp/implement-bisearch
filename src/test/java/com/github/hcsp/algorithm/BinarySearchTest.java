1e4ad825080210c550ef243da8cb795d73141de2
wanghongkun <wanghongkun@corp.netease.com>
2019-09-24 14:59:54 +0800

    对测试用例中的字符串数组进行字典序排序

--- src/test/java/com/github/hcsp/algorithm/BinarySearchTest.java
+++ src/test/java/com/github/hcsp/algorithm/BinarySearchTest.java
@@ -3,11 +3,14 @@ package com.github.hcsp.algorithm;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 public class BinarySearchTest {
     @Test
     public void test() {
         String[] array = "a quick brown fox jumps over the lazy dog".split(" ");
-        Assertions.assertEquals(6, BinarySearch.binarySearch(array, "the"));
+        Arrays.sort(array); // array = [a, brown, dog, fox, jumps, lazy, over, quick, the]
+        Assertions.assertEquals(8, BinarySearch.binarySearch(array, "the"));
         Assertions.assertEquals(-1, BinarySearch.binarySearch(array, "aaa"));
     }
 }
