85fe85a1d1e918d7d14765ed59bfa8c214254d7c
Shankar Acharya <shankar.acharya524@gmail.com>
2019-10-12 23:37:47 +0545

    Add missing implementation for binary search (#9)
    Add missing implementation for binary search (#9)

--- src/main/java/com/github/hcsp/algorithm/BinarySearch.java
+++ src/main/java/com/github/hcsp/algorithm/BinarySearch.java
@@ -2,14 +2,33 @@ package com.github.hcsp.algorithm;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        System.out.println(binarySearch(new String[] {"aaa", "ccc", "fff", "yyy", "zzz"}, "bbb"));
-        System.out.println(binarySearch(new String[] {"aaa", "ccc", "fff", "yyy", "zzz"}, "yyy"));
+        print(binarySearch(new String[] {"aaa", "ccc", "fff", "yyy", "zzz"}, "bbb"));
+        print(binarySearch(new String[] {"aaa", "ccc", "fff", "yyy", "zzz"}, "yyy"));
     }
 
     // 给定一个按照字符串升序升序排好序的用户数组，寻找目标字符串的位置，返回其索引值
     // 如果未找到，返回-1
     // 我们鼓励你使用递归和非递归两种方式
     public static int binarySearch(String[] strings, String target) {
+        int first = 0;
+        int last = strings.length - 1;
+        int mid;
+
+        while (last >= first) {
+            mid = (first + last) / 2;
+
+            if (strings[mid].equals(target)) {
+                return mid;
+            } else if (strings[mid].compareTo(target) > 0) {
+                last = mid - 1;
+            } else {
+                first = mid + 1;
+            }
+        }
         return -1;
+    }
+
+    public static void print(int number) {
+        System.out.println(number);
     }
 }
