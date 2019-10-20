0d7daf141a609a26f30f7a16d165c7f763a22e16
Hana <384073877@qq.com>
2019-10-20 17:38:44 +0800

    递归的二分查找

--- src/main/java/com/github/hcsp/algorithm/BinarySearch.java
+++ src/main/java/com/github/hcsp/algorithm/BinarySearch.java
@@ -9,10 +9,7 @@ public class BinarySearch {
     // 给定一个按照字符串升序升序排好序的用户数组，寻找目标字符串的位置，返回其索引值
     // 如果未找到，返回-1
     // 我们鼓励你使用递归和非递归两种方式
-    public static int binarySearch(String[] strings, String target) {
-        int end = strings.length - 1;
-        int start = 0;
-
+    public static int result(String[] strings, String target, int start, int end) {
         while (start < end) {
             if (strings[start].equals(target)) {
                 return start;
@@ -20,14 +17,20 @@ public class BinarySearch {
                 return end;
             }
             int mid = (end - start) / 2;
+
             if (strings[mid].equals(target)) {
                 return mid;
             } else if (strings[mid].compareTo(target) < 0) {
-                start = mid + 1;
+                return result(strings, target, mid + 1, end);
             } else if (strings[mid].compareTo(target) > 0) {
-                end = mid - 1;
+                return result(strings, target, start, mid - 1);
             }
         }
         return -1;
+    }
+
+    public static int binarySearch(String[] strings, String target) {
+        //非递归
+        return result(strings, target, 0, strings.length - 1);
     }
 }
