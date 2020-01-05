5f69eb9bc8b680104cb9532711bab8c23d17bbe5
baixc <969552017@qq.com>
2020-01-05 10:05:25 +0800

    使用循环进行二分查找

--- src/main/java/com/github/hcsp/algorithm/BinarySearch.java
+++ src/main/java/com/github/hcsp/algorithm/BinarySearch.java
@@ -2,14 +2,30 @@ package com.github.hcsp.algorithm;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        System.out.println(binarySearch(new String[] {"aaa", "ccc", "fff", "yyy", "zzz"}, "bbb"));
-        System.out.println(binarySearch(new String[] {"aaa", "ccc", "fff", "yyy", "zzz"}, "yyy"));
+        System.out.println(binarySearch(new String[]{"aaa", "ccc", "fff", "yyy", "zzz"}, "bbb"));
+        System.out.println(binarySearch(new String[]{"aaa", "ccc", "fff", "yyy", "zzz"}, "yyy"));
     }
 
     // 给定一个按照字符串升序升序排好序的用户数组，寻找目标字符串的位置，返回其索引值
     // 如果未找到，返回-1
     // 我们鼓励你使用递归和非递归两种方式
     public static int binarySearch(String[] strings, String target) {
+        return binarySearchByLoop(strings, target);
+    }
+
+    private static int binarySearchByLoop(String[] strings, String target) {
+        int low = 0;
+        int high = strings.length - 1;
+        while (high > low) {
+            int mid = low + (high - low);
+            if (strings[mid].equals(target)) {
+                return mid;
+            } else if (strings[mid].compareTo(target) < 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
         return -1;
     }
 }
