cf6215c949006d06fd145a4b4666041bac1b6af5
PingAn_NetBar <840495823@qq.com>
2020-01-20 00:11:45 +0800

    Update BinarySearch.java

--- src/main/java/com/github/hcsp/algorithm/BinarySearch.java
+++ src/main/java/com/github/hcsp/algorithm/BinarySearch.java
@@ -3,13 +3,12 @@ package com.github.hcsp.algorithm;
 public class BinarySearch {
     public static void main(String[] args) {
         String[] A1 = {"aaa", "ccc", "fff", "yyy", "zzz"};
-        String[] A2 = {"aaa", "ccc", "fff", "yyy", "zzz"};
 
         System.out.println(binarySearch(A1, "bbb"));
-        System.out.println(binarySearch(A2,"yyy"));
+        System.out.println(binarySearch(A1, "yyy"));
 
         System.out.println(binarySearchByRecursion(A1, "bbb", 0, A1.length-1));
-        System.out.println(binarySearchByRecursion(A2, "yyy", 0, A2.length-1));
+        System.out.println(binarySearchByRecursion(A1, "yyy", 0, A2.length-1));
     }
 
     // 给定一个按照字符串升序升序排好序的用户数组，寻找目标字符串的位置，返回其索引值
