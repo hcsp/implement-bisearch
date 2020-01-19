ad54df37d4ecfbbd0aed8c75a42e2d910f8e1996
Jason <840495823@qq.com>
2020-01-19 23:26:46 +0800

    注释鼓励去使用递归和非递归两种方式，但多次尝试和搜索发现递归二分查找确实需要把起始下标和终点下标作为递归方法的形式参数。但若main方法中直接使用匿名数组，则终点下标就无法正常取到数组的length-1，故此把两个数组重新New了一遍，让递归二分查找变得可行。

--- src/main/java/com/github/hcsp/algorithm/BinarySearch.java
+++ src/main/java/com/github/hcsp/algorithm/BinarySearch.java
@@ -2,40 +2,23 @@ package com.github.hcsp.algorithm;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        System.out.println(binarySearch(new String[] {"aaa", "ccc", "fff", "yyy", "zzz"}, "bbb"));
-        System.out.println(binarySearch(new String[] {"aaa", "ccc", "fff", "yyy", "zzz"}, "yyy"));
+        String[] A1 = {"aaa", "ccc", "fff", "yyy", "zzz"};
+        String[] A2 = {"aaa", "ccc", "fff", "yyy", "zzz"};
+
+        System.out.println(binarySearch(A1, "bbb"));
+        System.out.println(binarySearch(A2,"yyy"));
     }
 
     // 给定一个按照字符串升序升序排好序的用户数组，寻找目标字符串的位置，返回其索引值
     // 如果未找到，返回-1
     // 我们鼓励你使用递归和非递归两种方式
     public static int binarySearch(String[] strings, String target) {
-        int start = 0;
-        int end   = strings.length-1;
 
-        while (start <= end){
-            int mid = start + (end - start)/2;
-            if (strings[mid].compareTo(target)<0){
-                start = mid + 1;
-            }else if (strings[mid].compareTo(target)>0){
-                end = mid -1;
-            }else {
-                return mid;
-            }
-        }
         return -1;
     }
 
     public static int binarySearchByRecursion(String[] strings, String target, int start, int end){
 
-        while (start <= end){
-            int mid = start + (end - start)/2;
-            if (strings[mid].compareTo(target)<0){
-                return binarySearchByRecursion(strings, target, mid+1, end);
-            }else if (strings[mid].compareTo(target)>0){
-                return binarySearchByRecursion(strings, target, start, mid-1);
-            }
-        }
         return -1;
     }
 
