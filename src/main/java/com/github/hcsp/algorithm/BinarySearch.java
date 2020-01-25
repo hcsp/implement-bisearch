4a56d10cf774333a2c17c9b29082f273cefd4385
NervousOrange <zch693922442@outlook.com>
2020-01-25 21:22:49 +0100

    from NervousOrange: Assignment 1 of  basic Algorithm.

--- src/main/java/com/github/hcsp/algorithm/BinarySearch.java
+++ src/main/java/com/github/hcsp/algorithm/BinarySearch.java
@@ -10,6 +10,30 @@ public class BinarySearch {
     // 如果未找到，返回-1
     // 我们鼓励你使用递归和非递归两种方式
     public static int binarySearch(String[] strings, String target) {
-        return -1;
+        if (target.compareTo(strings[0]) < 0 || target.compareTo(strings[strings.length - 1]) > 0) {
+            return -1;
+        }
+        return binarySearchHelper(strings, target, 0, strings.length - 1);
+    }
+
+    public static int binarySearchHelper(String[] strings, String target, int start, int end) {
+        int middle = (start + end) / 2;
+        if (target.compareTo(strings[start]) == 0) {
+            return start;
+        }
+        if (target.compareTo(strings[end]) == 0) {
+            return end;
+        }
+        if (target.compareTo(strings[middle]) == 0) {
+            return middle;
+        } else if (end - start == 1) {
+            return -1;
+        }
+        if (target.compareTo(strings[middle]) < 0) {
+            return binarySearchHelper(strings, target, start, middle);
+        } else {
+            return  binarySearchHelper(strings, target, middle, end);
+        }
+
     }
 }
