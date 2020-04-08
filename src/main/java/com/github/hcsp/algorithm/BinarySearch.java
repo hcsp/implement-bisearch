ea90814ea26c312861c738eb006fc4bca37d8e2c
samuel <zhangleven@gmail.com>
2020-04-09 00:17:10 +0800

    impl binarySearch and recursiveBinarySearch

--- src/main/java/com/github/hcsp/algorithm/BinarySearch.java
+++ src/main/java/com/github/hcsp/algorithm/BinarySearch.java
@@ -10,6 +10,38 @@ public class BinarySearch {
     // 如果未找到，返回-1
     // 我们鼓励你使用递归和非递归两种方式
     public static int binarySearch(String[] strings, String target) {
+        int low = 0;
+        int high = strings.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            String midStr = strings[mid];
+            int compare = target.compareTo(midStr);
+
+            if (compare < 0) {
+                high = mid - 1;
+            } else if (compare > 0) {
+                low = mid + 1;
+            } else  {
+                return mid;
+            }
+        }
         return -1;
+    }
+
+    public static int recursiveBinarySearch(String[] strings, String target, int low, int high) {
+        if (low > high) return -1;
+        int mid = (low + high) / 2;
+        String midStr = strings[mid];
+
+        int compare = target.compareTo(midStr);
+        if (compare < 0) {
+            high = mid - 1;
+        } else if (compare > 0) {
+            low = mid + 1;
+        } else {
+            return mid;
+        }
+
+        return recursiveBinarySearch(strings, target, low, high);
     }
 }
