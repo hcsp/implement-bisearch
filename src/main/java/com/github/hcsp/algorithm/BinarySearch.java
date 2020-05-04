318ff8e00ed153fefb31c403c7d691146df34ea5
gofocus <go_focus@live.cn>
2020-05-04 13:39:53 +0800

    二分查找 (#68)
    二分查找 (#68)

--- src/main/java/com/github/hcsp/algorithm/BinarySearch.java
+++ src/main/java/com/github/hcsp/algorithm/BinarySearch.java
@@ -2,14 +2,71 @@ package com.github.hcsp.algorithm;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        System.out.println(binarySearch(new String[] {"aaa", "ccc", "fff", "yyy", "zzz"}, "bbb"));
-        System.out.println(binarySearch(new String[] {"aaa", "ccc", "fff", "yyy", "zzz"}, "yyy"));
+        System.out.println(binarySearch(new String[]{"aaa", "ccc", "fff", "yyy", "zzz"}, "bbb"));
+        System.out.println(binarySearch(new String[]{"aaa", "ccc", "fff", "yyy", "zzz"}, "yyy"));
+        System.out.println(binarySearch2(new String[]{"aaa", "ccc", "fff", "yyy", "zzz"}, "bbb"));
+        System.out.println(binarySearch2(new String[]{"aaa", "ccc", "fff", "yyy", "zzz"}, "yyy"));
     }
 
     // 给定一个按照字符串升序升序排好序的用户数组，寻找目标字符串的位置，返回其索引值
     // 如果未找到，返回-1
     // 我们鼓励你使用递归和非递归两种方式
     public static int binarySearch(String[] strings, String target) {
-        return -1;
+        int start = 0, end = strings.length - 1;
+
+        while (true) {
+            int mid = (start + end) / 2;
+            if (start == mid || end == mid) {
+                return -1;
+            }
+            if (strings[start].compareTo(target) > 0 || strings[end].compareTo(target) < 0) {
+                return -1;
+            } else if (strings[start].equals(target)) {
+                return start;
+            } else if (strings[end].equals(target)) {
+                return end;
+            }
+
+            int midCompareWithTarget = strings[mid].compareTo(target);
+            if (midCompareWithTarget < 0) {
+                start = mid;
+            } else if (midCompareWithTarget > 0) {
+                end = mid;
+            } else {
+                return mid;
+            }
+        }
     }
+
+    public static int binarySearch2(String[] strings, String target) {
+        return searchRecursive(strings, target, 0, strings.length - 1);
+    }
+
+    public static int searchRecursive(String[] strings, String target, int start, int end) {
+        int mid = (start + end) / 2;
+        int targetCompare2Start = target.compareTo(strings[start]);
+        int targetCompare2End = target.compareTo(strings[end]);
+        if (start == mid || end == mid) {
+            return -1;
+        }
+        if (targetCompare2Start < 0 || targetCompare2End > 0) {
+            return -1;
+        } else if (targetCompare2Start == 0) {
+            return start;
+        } else if (targetCompare2End == 0) {
+            return end;
+        }
+
+        int compareTo = target.compareTo(strings[mid]);
+        if (compareTo < 0) {
+            end = mid;
+            return searchRecursive(strings, target, start, end);
+        } else if (compareTo > 0) {
+            start = mid;
+            return searchRecursive(strings, target, start, end);
+        } else {
+            return mid;
+        }
+    }
+
 }
