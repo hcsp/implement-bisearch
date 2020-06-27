7418f185faba1be50b0ce52e81c443b7bcbd0048
豪 <linesahao@outlook.com>
2020-06-27 19:43:18 +0800

    二分查找算法。 (#80)
    二分查找算法。 (#80)

--- src/main/java/com/github/hcsp/algorithm/BinarySearch.java
+++ src/main/java/com/github/hcsp/algorithm/BinarySearch.java
@@ -2,14 +2,56 @@ package com.github.hcsp.algorithm;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        System.out.println(binarySearch(new String[] {"aaa", "ccc", "fff", "yyy", "zzz"}, "bbb"));
-        System.out.println(binarySearch(new String[] {"aaa", "ccc", "fff", "yyy", "zzz"}, "yyy"));
+        System.out.println(binarySearch(new String[]{"aaa", "ccc", "fff", "yyy", "zzz"}, "bbb"));
+        System.out.println(binarySearch(new String[]{"aaa", "ccc", "fff", "yyy", "zzz"}, "yyy"));
     }
 
     // 给定一个按照字符串升序升序排好序的用户数组，寻找目标字符串的位置，返回其索引值
     // 如果未找到，返回-1
     // 我们鼓励你使用递归和非递归两种方式
     public static int binarySearch(String[] strings, String target) {
-        return -1;
+        int start = 0, end = strings.length - 1;
+        while (true) {
+            if (target.equals(strings[start])) {
+                return start;
+            }
+            if (target.equals(strings[end])) {
+                return end;
+            }
+            int mid = (end + start) / 2;
+            if (mid == 0) {
+                return -1; // 最终没有找到
+            }
+            String midStr = strings[mid];
+            if (midStr.compareTo(target) > 0) {
+                end = mid;
+            } else if (midStr.compareTo(target) < 0) {
+                start = mid;
+            }else {
+                return mid;
+            }
+        }
+    }
+
+    public static int binarySearchRecursive(String[] strings, String target, int start, int end) {
+        // 结束条件
+        if (target.equals(strings[start])) {
+            return start;
+        }
+        if (target.equals(strings[end])) {
+            return end;
+        }
+        int mid = (start + end) / 2;
+        if (mid == 0) {
+            return -1;
+        }
+        String midStr = strings[mid];
+        if (midStr.compareTo(target) > 0) {
+            return binarySearchRecursive(strings, target, start, mid);
+        } else if (midStr.compareTo(target) < 0) {
+            return binarySearchRecursive(strings, target, mid, end);
+        } else {
+            return mid;
+        }
     }
 }
