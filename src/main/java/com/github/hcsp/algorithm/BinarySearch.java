f73596d9bfc3f1fc399e5ac1ed6df76aa0978a94
sgz886 <sgz886@gmail.com>
2020-12-17 22:48:55 +0800

    二分查找 (#98)
    二分查找 (#98)

--- src/main/java/com/github/hcsp/algorithm/BinarySearch.java
+++ src/main/java/com/github/hcsp/algorithm/BinarySearch.java
@@ -10,6 +10,27 @@ public class BinarySearch {
     // 如果未找到，返回-1
     // 我们鼓励你使用递归和非递归两种方式
     public static int binarySearch(String[] strings, String target) {
+        int start = 0;
+        int end = strings.length - 1;
+        int position;
+        while (start <= end) {
+            position = (start + end) / 2;
+            if (strings[position].compareTo(target) > 0) {
+                System.out.printf("查找记录: start=%d end=%d position =%d %s > %s%n", start, end, position,
+                        strings[position], target);
+                end = position - 1;
+            } else if (strings[position].compareTo(target) < 0) {
+                System.out.printf("查找记录: start=%d end=%d position =%d %s < %s%n", start, end, position,
+                        strings[position], target);
+                start = position + 1;
+            } else {
+                System.out.printf("查找记录: start=%d end=%d position =%d %s = %s%n", start, end, position,
+                        strings[position], target);
+                System.out.println("索引是" + position);
+                return position;
+            }
+        }
         return -1;
+
     }
 }
