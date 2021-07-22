75eb1e841b4216e2847a6dedefe4a8a5b40f9512
bowen-wu <13793248162@163.com>
2021-07-22 21:35:56 +0800

    feat: complete not recursion String.compareTo

--- src/main/java/com/github/hcsp/algorithm/BinarySearch.java
+++ src/main/java/com/github/hcsp/algorithm/BinarySearch.java
@@ -1,8 +1,5 @@
 package com.github.hcsp.algorithm;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class BinarySearch {
     public static void main(String[] args) {
         System.out.println(binarySearch(new String[]{"aaa", "ccc", "fff", "yyy", "zzz"}, "bbb"));
@@ -13,39 +10,29 @@ public class BinarySearch {
     // 如果未找到，返回-1
     // 我们鼓励你使用递归和非递归两种方式
     public static int binarySearch(String[] strings, String target) {
-        return binarySearch(strings, target, 0);
-    }
-
-    public static int binarySearch(String[] strings, String target, int lastStart) {
         int start = 0, end = strings.length - 1;
-        char targetInitial = target.charAt(0);
-        char startInitial = strings[start].charAt(0);
-        char endInitial = strings[end].charAt(0);
-        int mid = (start + end) / 2;
-        char midInitial = strings[mid].charAt(0);
-        if (strings[start].equals(target)) {
-            return start;
-        }
+        while (true) {
+            if (strings[start].equals(target)) {
+                return start;
+            }
 
-        if (strings[end].equals(target)) {
-            return end;
-        }
-        if (startInitial > targetInitial || endInitial < targetInitial || end == start + 1) {
-            return -1;
-        }
+            if (strings[end].equals(target)) {
+                return end;
+            }
 
-        List<String> stringList = Arrays.asList(strings);
-        if (midInitial > targetInitial) {
-            String[] newStrings = new String[mid - start];
-            newStrings = stringList.subList(start, mid + 1).toArray(newStrings);
-            return binarySearch(newStrings, target, start);
-        } else if (midInitial < targetInitial) {
-            String[] newStrings = new String[end - mid];
-            newStrings = stringList.subList(mid, end + 1).toArray(newStrings);
-            return binarySearch(newStrings, target, mid);
-        } else {
-            return mid + lastStart;
+            int mid = (start + end) / 2;
+
+            if (mid == start) {
+                return -1;
+            }
+
+            if (strings[mid].compareTo(target) > 0) {
+                end = mid;
+            } else if (strings[mid].compareTo(target) < 0) {
+                start = mid;
+            } else {
+                return mid;
+            }
         }
     }
-
 }
