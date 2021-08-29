5cd1f5d67f18e04824aeafd7b67a0b2d70ddf408
skysZhang <37018639+skysZhang@users.noreply.github.com>
2021-08-29 09:55:51 +0800

    recursive bisearch (#118)
    recursive bisearch (#118)

--- src/main/java/com/github/hcsp/algorithm/BinarySearch.java
+++ src/main/java/com/github/hcsp/algorithm/BinarySearch.java
@@ -2,14 +2,41 @@ package com.github.hcsp.algorithm;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        System.out.println(binarySearch(new String[] {"aaa", "ccc", "fff", "yyy", "zzz"}, "bbb"));
-        System.out.println(binarySearch(new String[] {"aaa", "ccc", "fff", "yyy", "zzz"}, "yyy"));
+        System.out.println(binarySearch(new String[]{"aaa", "ccc", "fff", "yyy", "zzz"}, "bbb"));
+        System.out.println(binarySearch(new String[]{"aaa", "ccc", "fff", "yyy", "zzz"}, "yyy"));
     }
+
 
     // 给定一个按照字符串升序升序排好序的用户数组，寻找目标字符串的位置，返回其索引值
     // 如果未找到，返回-1
     // 我们鼓励你使用递归和非递归两种方式
+
+    public static int main(String[] strings, String target) {
+        return binarySearch(strings, target);
+    }
+
     public static int binarySearch(String[] strings, String target) {
+        int l = 0, r = strings.length - 1;
+        int mid = (l + r) / 2;
+        if (strings[l].equals(target)) {
+            return l;
+        }
+        if (strings[r].equals(target)) {
+            return r;
+        }
+
+        if (mid == l) {
+            return -1;
+        }
+
+        if (strings[mid].compareTo(target) > 0) {
+            r = mid;
+        } else if (strings[mid].compareTo(target) < 0) {
+            l = mid;
+        } else {
+            return mid;
+        }
         return -1;
     }
 }
+
