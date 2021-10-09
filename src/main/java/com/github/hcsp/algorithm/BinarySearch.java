ad8a8ddfa0cc74eef166591e4a0646593554e5da
jianqi <jianqi.zhao.xian@gmail.com>
2021-10-09 22:53:13 +1100

    job done

--- src/main/java/com/github/hcsp/algorithm/BinarySearch.java
+++ src/main/java/com/github/hcsp/algorithm/BinarySearch.java
@@ -1,8 +1,10 @@
 package com.github.hcsp.algorithm;
 
+import java.util.Arrays;
+
 public class BinarySearch {
     public static void main(String[] args) {
-        System.out.println(binarySearch(new String[] {"aaa", "ccc", "fff", "yyy", "zzz"}, "bbb"));
+        System.out.println(binarySearch(new String[] {"aaa", "ccc", "fff", "yyy", "zzz"}, "zzz"));
         System.out.println(binarySearch(new String[] {"aaa", "ccc", "fff", "yyy", "zzz"}, "yyy"));
     }
 
@@ -10,6 +12,22 @@ public class BinarySearch {
     // 如果未找到，返回-1
     // 我们鼓励你使用递归和非递归两种方式
     public static int binarySearch(String[] strings, String target) {
+        if (strings[0].equals(target)) {
+            return 0;
+        } else if (strings[strings.length-1].equals(target)) {
+            return strings.length-1;
+        } else if (strings.length <= 2) {
+            return -1;
+        } else {
+            int mid = strings.length/2;
+            if (strings[mid].equals(target)) {
+                return mid;
+            } else if (strings[mid].compareTo(target) < 0) {
+                return mid + 1 + binarySearch(Arrays.copyOfRange(strings, mid+1, strings.length), target);
+            } else if (strings[mid].compareTo(target) > 0) {
+                return binarySearch(Arrays.copyOfRange(strings, 0, mid), target);
+            }
+        }
         return -1;
     }
 }
